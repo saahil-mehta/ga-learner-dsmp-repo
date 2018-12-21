5b40d5c8ee165ca80f38ca9ef195dcc200f7d334
Saahil Mehta <sahil.mehta8520@gmail.com>
2018-12-22 00:04:20 +0530

    Code file created.

---
+++ Project--Make-Sense-of-Census/code.py
@@ -0,0 +1,69 @@
+# --------------
+# Importing header files
+import numpy as np
+
+# Path of the file has been stored in variable called 'path'
+dfile='path'
+data=np.genfromtxt(path,delimiter=",", skip_header=1)
+data
+#New record
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+census=np.concatenate([new_record,data])
+
+
+
+# --------------
+#Code starts here
+age = census[:,0]
+max_age=age.max()
+max_age
+min_age=age.min()
+min_age
+age_mean=age.mean()
+age_mean
+age_std=age.std()
+age_std
+
+
+# --------------
+#Code starts here
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
+len_0=len(race_0)
+len_1=len(race_1)
+len_2=len(race_2)
+len_3=len(race_3)
+len_4=len(race_4)
+listmin=(len_0,len_1,len_2,len_3,len_4)
+minority_race=listmin.index(min(listmin))
+
+
+# --------------
+#Code starts here
+senior_citizens=census[census[:,0]>60]
+
+#Calculating the sum of all the values of array
+working_hours_sum=senior_citizens.sum(axis=0)[6]
+
+#Finding the length of the array
+senior_citizens_len=len(senior_citizens)
+
+#Finding the average working hours
+avg_working_hours=working_hours_sum/senior_citizens_len
+
+#Printing the average working hours
+print((avg_working_hours))
+
+
+# --------------
+#Code starts here
+high=census[census[:,1]>10]
+low=census[census[:,1]<=10]
+avg_pay_high=high[:,7].mean()
+avg_pay_low=low[:,7].mean()
+avg_pay_high,avg_pay_low
+
+
